738f53330dae7a4f1923cc512fe30de36ac441ca
sabu <2024.sabuhi.abbasov@uwcisak.jp>
2023-09-01 15:11:11 +0900

    Update quiz_074.py

--- Quizzes/Codes/quiz_074.py
+++ Quizzes/Codes/quiz_074.py
@@ -1,1 +1,18 @@
-def build_data_pkg(mac_rx: str)
+def build_data_pkg(mac_rx: str, ip_rx: str, mac_sx: str, ip_sx: str, data: str):
+    ip_mac = f"{mac_rx}|{ip_rx}|{mac_sx}|{ip_sx}|"
+    output = []
+    length = len(data)
+    if length % 4 != 0:
+        length = length // 4 + 1
+    for i in range(length):
+        load = data[:4]
+        if len(load) < 4:
+            load = data
+        data = data[4:]
+        output.append(f"{ip_mac}{i}|{load}")
+
+    return output
+
+
+data_package = build_data_pkg("8C:31:03:4F:CE:DB", "192.168.0.1", "7A:7A:A7:C2:44:FB", "192.168.0.2", "Hello world")
+print(data_package)
