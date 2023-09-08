68e4458398d928ab7a855849a4273745203d519b
sabu <2024.sabuhi.abbasov@uwcisak.jp>
2023-09-08 13:05:25 +0900

    Update quiz_076.py

--- Quizzes/Codes/quiz_076.py
+++ Quizzes/Codes/quiz_076.py
@@ -8,24 +8,25 @@ class checker:
         data_arr = []
         for letter in self.data:
             data_arr.append(letter)
-        original = data_arr[0:length]
-        copy_1 = data_arr[length:length*2]
-        copy_2 = data_arr[length*2:length*3]
-        for i in range(0, length):
-            if original[i] == copy_1[i] and copy_2[i] != original[i]:
-                self.error = True
-                pass
-            elif original[i] == copy_2[i] and copy_1[i] != original[i]:
-                self.error = True
-                pass
-            elif copy_1[i] == copy_2[i] and original[i] != copy_2[i]:
+        original, copy_1, copy_2 = data_arr[:length], data_arr[length:length * 2], data_arr[length * 2:length * 3]
+        # for i in range(0, length):
+        #     if original[i] == copy_1[i] and copy_2[i] != original[i]:
+        #         self.error = True
+        #         pass
+        #     elif original[i] == copy_2[i] and copy_1[i] != original[i]:
+        #         self.error = True
+        #         pass
+        #     elif copy_1[i] == copy_2[i] and original[i] != copy_2[i]:
+        #         self.error = True
+        #         original[i] = copy_1[i]
+
+        for i in range(length):
+            if (original[i] == copy_1[i] or original[i] == copy_2[i]) and copy_1[i] != copy_2[i]:
                 self.error = True
                 original[i] = copy_1[i]
-        output = ''
-        for binary in original:
-            output = output + str(binary)
-        return self.error, output
 
-error_checker = checker("101010101011")
+        return self.error, ''.join(original)
+
+error_checker = checker("101010101010")
 output = error_checker.error_check()
 print(output)
