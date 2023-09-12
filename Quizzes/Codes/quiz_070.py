90bafa1ba343ad4d6806712a0f40e3abf7fdf6b9
sabu <2024.sabuhi.abbasov@uwcisak.jp>
2023-09-12 13:53:30 +0900

    Update quiz_070

--- Quizzes/Codes/quiz_070.py
+++ Quizzes/Codes/quiz_070.py
@@ -1,15 +1,27 @@
-# Define a class named 'ipv4machine'
-class ipv4machine:
-    # Method 'generator' generates a list of IPv4 addresses in the format "i.j.k.l" for all valid combinations
-    def generator(self):
-        # Use list comprehensions to generate all possible IPv4 addresses
-        return [f"{i}.{j}.{k}.{l}" for i in range(256) for j in range(256) for k in range(256) for l in range(256)]
+# Function to generate IPv4 addresses
+def generate_ipv4():
+    addresses = []
+    total_addresses = 256 ** 4
 
-# Create an instance of the 'ipv4machine' class
-ipv4 = ipv4machine()
+    for i in range(256):
+        for j in range(256):
+            for k in range(256):
+                for l in range(256):
+                    address = f"{i}.{j}.{k}.{l}"
+                    addresses.append(address)
 
-# Call the 'generator' method to generate a list of IPv4 addresses
-output = ipv4.generator()
+                    # Print the first 10 generated addresses as an example
+                    if len(addresses) <= 20:
+                        print(f"Generated IP: {address}")
+
+    return addresses
+
+
+# Call the function to generate IPv4 addresses
+output = generate_ipv4()
+
+# Print a new line to separate progress from the final result
+print("\n")
 
 # Print the list of generated IPv4 addresses
 print(output)
