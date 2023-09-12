f546935821e0b14005e884dfdd4271cfe52a0ed8
sabu <2024.sabuhi.abbasov@uwcisak.jp>
2023-09-12 13:46:21 +0900

    Update quiz_069

--- Quizzes/Codes/quiz_069.py
+++ Quizzes/Codes/quiz_069.py
@@ -1,17 +1,29 @@
-# Define a function that takes a list of sales and returns the difference between the highest and lowest profits.
-def profit(sales: list) -> int:
-    min_profit = 99999999  # The highest number to compare with the profits
-    max_profit = -1  # The lowest number to compare with the profits
-    for profits in sales:  # Loop through the list of sales
-        if profits < min_profit:  # If the profits is less than the min_profit
-            min_profit = profits  # Assign the profits to the min_profit
-        if profits > max_profit:  # If the profits is greater than the max_profit
-            max_profit = profits  # Assign the profits to the max_profit
-    difference = max_profit - min_profit  # Calculate the difference between the highest and lowest profits
-    return difference  # Return the difference
+class ProfitCalculator:
+    def __init__(self, sales):
+        self.sales = sales
+
+    def calculate_profit_difference(self):
+        min_profit = 99999999  # Initialize the minimum profit with a high value
+        max_profit = -1  # Initialize the maximum profit with a low value
+
+        for profit in self.sales:  # Loop through the list of profits
+            if profit < min_profit:  # If the profit is less than the current minimum
+                min_profit = profit  # Update the minimum profit
+            if profit > max_profit:  # If the profit is greater than the current maximum
+                max_profit = profit  # Update the maximum profit
+
+        difference = max_profit - min_profit  # Calculate the difference between the highest and lowest profits
+        return difference  # Return the difference
+
+    def custom_message(self):
+        difference = self.calculate_profit_difference()
+        return f"The difference between the highest and lowest profits: {difference}"
 
 
-sales = [100, 45, 12, 3, 56, 7]  # List of sales
+# Example usage
+sales = [100, 45, 12, 3, 56, 7]
+profit_calculator = ProfitCalculator(sales)  # Create an instance of the ProfitCalculator class
 
-output = profit(sales)  # Call the function and assign the return value to the output variable
-print(f"The difference between the highest and lowest profits: {output}")  # Output the result
+# Get the custom message and print it
+message = profit_calculator.custom_message()
+print(message)  # Output the custom message with the result
