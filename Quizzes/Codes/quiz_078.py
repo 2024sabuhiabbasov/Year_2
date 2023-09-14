fffc57df160b2306344e368adef2e534e54b5566
sabu <2024.sabuhi.abbasov@uwcisak.jp>
2023-09-14 13:48:01 +0900

    Update quiz_078.py

--- Quizzes/Codes/quiz_078.py
+++ Quizzes/Codes/quiz_078.py
@@ -3,16 +3,25 @@ class layer4_firewall:
         self.data = data
 
     def msg(self):
-        port_bits = self.data[:16]  # Extract the first 16 bits as the port number
-        port_number = int(port_bits, 2)  # Convert the first 16 bits to an integer
+        # Extract the first 16 bits as the port number
+        port_bits = self.data[:16]
+
+        # Convert the first 16 bits to an integer
+        port_number = int(port_bits, 2)
         print(port_number)
+
+        # Check if the port number is 80 or 22123
         if port_number == 80 or port_number == 22123:
             return "Allowed", self.data[16:]
         else:
             return "Filtered", "None"
 
 
+# Create an instance of the layer4_firewall class with the binary data
 layer_4 = layer4_firewall("00000000010100001011")  # 80
 
+# Call the msg method and retrieve the status and message
 status, message = layer_4.msg()
+
+# Print the status and message
 print(f"{status}, {message}")
