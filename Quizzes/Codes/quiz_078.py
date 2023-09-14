d5986ec4feb43f3cbea1870dd41fad95d2e68183
sabu <2024.sabuhi.abbasov@uwcisak.jp>
2023-09-14 13:47:10 +0900

    Update quiz_078.py

--- Quizzes/Codes/quiz_078.py
+++ Quizzes/Codes/quiz_078.py
@@ -1,30 +1,18 @@
 class layer4_firewall:
     def __init__(self, data: str):
         self.data = data
-        self.error = True
-        self.message = 'None'
-        self.length = len(data)
-
-    def converter(self, data: str):
-        decimal = 0
-        power = 15
-        for i in range(self.length - 1, self.length - 17, -1):
-            integer = int(data[i])
-            decimal += integer * (2 ** power)
-            power -= 1
-        return decimal
 
     def msg(self):
-        decimal = self.converter(self.data)
-        if decimal == 80 or decimal == 22123:
-            self.error = False
-        if self.error:
-            self.message = 'None'
-            return "Filtered", self.message
+        port_bits = self.data[:16]  # Extract the first 16 bits as the port number
+        port_number = int(port_bits, 2)  # Convert the first 16 bits to an integer
+        print(port_number)
+        if port_number == 80 or port_number == 22123:
+            return "Allowed", self.data[16:]
         else:
-            return "Allowed", self.data[16: self.length]
+            return "Filtered", "None"
 
 
-layer_4 = layer4_firewall("0000000010100001111")
+layer_4 = layer4_firewall("00000000010100001011")  # 80
 
-print(layer_4.msg())
+status, message = layer_4.msg()
+print(f"{status}, {message}")
