61c5c1343fd13cf9e54194254ebc46204e0f714e
sabu <2024.sabuhi.abbasov@uwcisak.jp>
2023-09-19 08:43:17 +0900

    Update collections.py

--- Class_Notes/collection.py
+++ Class_Notes/collection.py
@@ -10,8 +10,35 @@ class collection:
     def isEmpty(self) -> bool:
         return len(self.data) == 0
 
+    def hasNext(self) -> bool:
+        return self.location == len(self.data) - 1
+
+    def getNext(self):
+        end_code = "\033[00m"
+        red = "\033[0;31m"
+        if self.hasNext():
+            item = self.data[self.location]
+            self.location += 1
+        else:
+            item = f"{red}[ERROR] Collection out of index{end_code}"
+        return item
+
+    def resetNext(self):
+        self.location = 0
+
+    # My own method to attract customers
+    def removeLast(self):
+        self.data = self.data[0:len(self.data) - 1]
+        self.location -= 1
+
 
 # imagining  how the user would use our ADI
 x = collection()
 x.addItem('bob')
-print(x.isEmpty())
+print(x.hasNext())
+print(x.getNext())
+x.addItem('john')
+print(x.getNext())
+x.removeLast()
+print(x.getNext())
+
